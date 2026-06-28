b83650ecbaf7513b600ed266b11443d0d12a2f19
Soumya6969 <soumyamanna66@gmail.com>
2026-06-29 01:13:29 +0530

    Added About and Contact pages

--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'helpdesk/helpdesk.html')
+    return render(request, "helpdesk/helpdesk.html")
+
+
+def about(request):
+    return render(request, "helpdesk/about.html")
+
+
+def contact(request):
+    return render(request, "helpdesk/contact.html")
